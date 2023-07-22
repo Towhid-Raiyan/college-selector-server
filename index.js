747dde53db5a6a56b740e927038799c9cc47778b
Towhid-Raiyan <towhid.raiyan.com>
2023-07-23 02:36:41 +0600

    college route setup

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cq8nopc.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const collegeCollection = client.db("collegeSelectorDb").collection("college");
+
+    // all college
+    app.get("/allCollege", async (req, res) => {
+          
+        const allCollege = await collegeCollection.find().toArray();
+        res.send(allCollege);
+    });
+
+    // popular college
+    app.get("/popularCollege", async (req, res) => {
+        try {
+            const popularCollege = await collegeCollection
+                .find()
+                .sort({ college_ratings: -1 })
+                .limit(3)
+                .toArray();
+            res.json(popularCollege);
+        } catch (error) {
+            console.error("Error fetching popular college:", error);
+            res.status(500).json({ error: "Internal server error" });
+        }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get("/", (req, res) => {
+    res.send("Server is Running...");
+});
+
+app.listen(port, () => {
+    console.log(`Server Running on PORT:  ${port}`);
+});
